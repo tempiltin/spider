78537a79eb38496946bda2f72cdaac08e60541ef
Temurbek Shukurov <91739351+tempiltin@users.noreply.github.com>
2024-03-11 23:24:50 +0500

    Create crawler.py
    Create crawler.py

---
+++ crawler.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import requests
+
+def request(url):
+    try:
+        return  requests.get("http://"+url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+target_url = "samtuit.uz"
+with open("/root/PycharmProjects/pythonProject/crawler/subdomains.list", "r") as wordlist_file:
+     for line in wordlist_file:
+         word = line.strip()
+         # test_url = word +"."+ target_url # subdomain uchun
+         test_url = target_url + "/" + word
+         respone = request(test_url)
+
+         if respone:
+             print("[+] Aniqlangan subdomain ----------> " + test_url)
